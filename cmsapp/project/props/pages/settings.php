f9b299bae4127afaa9611653de01ccb58c2a0d26
Ese Curtis <47295790+EseCurtis@users.noreply.github.com>
2021-03-28 14:30:28 +0100

    Delete cmsapp directory

--- cmsapp/project/props/pages/settings.php
+++
@@ -1,9 +0,0 @@
-<div class="main__page">
-    <div class="page__content">
-        <h2>Settings</h2>
-        <br>
-        <div>
-        <a href="<?=_URL_?>/settings/change-password" class="nav__item">Change Password</a>
-        </div>
-    </div>
-</div>
